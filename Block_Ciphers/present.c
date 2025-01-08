8a6fdcdd69e4ac7c3620a0a585c7fd473c018572
KyleDOwens <KyleDOwens26@gmail.com>
2025-01-07 21:41:09 -0600

    Working PRESENT encryption

--- Block_Ciphers/present.c
+++ Block_Ciphers/present.c
@@ -28,21 +28,44 @@ void print_block(uint64_t block) {
     for (int i = 63; i >= 0; i--) {
         printf("%ld", (block >> i) & 0x0000000000000001);
     }
-    printf("\n\r");
+    printf("\n");
 }
 
 void print_halfblock(uint32_t halfblock) {
     for (int i = 31; i >= 0; i--) {
         printf("%d", (halfblock >> i) & 0x0000000000000001);
     }
-    printf("\n\r");
+    printf("\n");
 }
 
 void print_byte(uint8_t byte) {
     for (int i = 7; i >= 0; i--) {
         printf("%d", (byte >> i) & 0x01);
     }
-    printf("\n\r");
+    printf("\n");
+}
+
+void print_nibble(uint8_t byte) {
+    for (int i = 3; i >= 0; i--) {
+        printf("%d", (byte >> i) & 0x01);
+    }
+    printf("\n");
+}
+
+void print_round(uint8_t byte) {
+    for (int i = 4; i >= 0; i--) {
+        printf("%d", (byte >> i) & 0x01);
+    }
+    printf("\n");
+}
+
+void print_key(uint8_t* key) {
+    for (int i = KEYSIZE/8 - 1; i >= 0; i--) {
+        for (int j = 7; j >= 0; j--) {
+            printf("%d", (key[i] >> j) & 0x01);
+        }
+    }
+    printf("\n");
 }
 
 
@@ -58,7 +81,7 @@ uint64_t leftmost64(uint8_t *key) {
     uint64_t output = 0;
     for (int i = 0; i < 64/8; i++) {
         output <<= 8;
-        output |= key[KEYSIZE/8 - i];
+        output |= key[(KEYSIZE/8 - 1) - i];
     }
     return output;
 }
@@ -68,16 +91,25 @@ uint64_t leftmost64(uint8_t *key) {
  * @param key pointer to the key to rotate
  */
 void rotate_left(uint8_t *key) {
+    // printf("\toriginal key = "); print_key(key);
+
     // Store first bit for wrap-around
     uint8_t carry = (key[KEYSIZE/8 - 1] >> 7) & 0x01;
+    // printf("\tTop bit = %d\n", carry);
 
     // Rotate all bytes except first
     for (int i = KEYSIZE/8 - 1; i > 0; i--) {
+        // printf("\t[%d] Before = ", i); print_byte(key[i]);
         key[i] = (key[i] << 1) | ((key[i-1] >> 7) & 0x01);
+        // printf("\t[%d] After  = ", i); print_byte(key[i]);
     }
     
     // Rotate first byte with the wrap-around carry
+    // printf("\t[0] Before = "); print_byte(key[0]);
     key[0] = (key[0] << 1) | carry;
+    // printf("\t[0] After  = "); print_byte(key[0]);
+
+    // printf("\trotated_key = "); print_key(key);
 }
 
 /**
@@ -99,42 +131,73 @@ uint64_t* generate_round_keys(uint8_t *key) {
     // Step 4: [k66,k65,k64,k63,k62] = [k66,k65,k64,k63,k62] XOR round_counter
     // subkey[round_counter] = leftmost 64 bits
 
+    // printf("main key = "); print_key(key);
     uint64_t* subkeys = (uint64_t*)calloc(32, sizeof(uint64_t));
+
+    // Copy the key which will be altered while generating the subkeys
+    uint8_t *key_copy = calloc(KEYSIZE/8, sizeof(uint8_t));
+    memcpy(key_copy, key, KEYSIZE/8);
+    // printf("key_copy = "); print_key(key_copy);
 
     // Get the first subkey
     subkeys[0] = leftmost64(key); // Round key 1
+    printf("subkey[1] = "); print_block(subkeys[0]);
 
-    for (uint8_t round_counter = 2; round_counter <= 32; round_counter++) {
+    for (uint8_t round_counter = 1; round_counter <= 31; round_counter++) {
+        // printf("========================\nROUND %d\n========================\n", round_counter);
+        // printf("starting key = "); print_key(key);
         // Step 1: rotate key left by 61
         for (int i = 0; i < 61; i++) {
             rotate_left(key);
         }
+        // printf("[%d] rotated_key = ", round_counter); print_key(key);
+        // printf("[%d] key[msb_nibble] = ", round_counter); print_byte(key[KEYSIZE/8 - 1]);
 
         // Step 2: set 4 leftmost bits to s-box values for 80-bit key, or 8 leftmost bits for 128-bit key
-        uint8_t msb_nibble = key[KEYSIZE/8 -1] >> 4; // Get the MSB nibble as input to the s-box
-        key[KEYSIZE/8 -1] &= 0x0F; // Set the nibble to 0
-        key[KEYSIZE/8 -1] |= sbox[msb_nibble] << 4; // Set MSB nibble to the s-box value
+        uint8_t msb_nibble = key[KEYSIZE/8 - 1] >> 4; // Get the MSB nibble as input to the s-box
+        key[KEYSIZE/8 - 1] &= 0x0F; // Set the nibble to 0
+        key[KEYSIZE/8 - 1] |= sbox[msb_nibble] << 4; // Set MSB nibble to the s-box value
+
+        // printf("[%d] msb_nibble = ", round_counter); print_nibble(msb_nibble);
+        // printf("[%d] sbox[msb_nibble] = ", round_counter); print_nibble(sbox[msb_nibble]);
+        // printf("[%d] s-box key = ", round_counter); print_key(key);
+        // printf("[%d] new_key[msb_nibble] = ", round_counter); print_byte(key[KEYSIZE/8 - 1]);
 
         if (KEYSIZE == 128) {
-            uint8_t msb_nibble = key[KEYSIZE/8 -1] & 0x0F; // Get the 2nd MSB nibble as input to the s-box
-            key[KEYSIZE/8 -1] &= 0xF0; // Set the nibble to 0
-            key[KEYSIZE/8 -1] |= sbox[msb_nibble]; // Set 2nd MSB nibble to the s-box value
+            uint8_t msb_nibble = key[KEYSIZE/8 - 1] & 0x0F; // Get the 2nd MSB nibble as input to the s-box
+            key[KEYSIZE/8 - 1] &= 0xF0; // Set the nibble to 0
+            key[KEYSIZE/8 - 1] |= sbox[msb_nibble]; // Set 2nd MSB nibble to the s-box value
         }
 
         // Step 3: XOR the round_counter with the 4 bits from (KEYSIZE - 61) to (KEYSIZE - 61 - 4) for 80-bit key, or (KEYSIZE - 62) to (KEYSIZE - 62 - 4) for 128-bit key
         // Awkward since this goes across byte boundaries, do it in two steps
+
+        // printf("[%d] round_counter = ", round_counter); print_round(round_counter);
+        // printf("[%d] key[23-16] = ", round_counter); print_byte(key[19/8]);
+        // printf("[%d]                  ^^^^\n", round_counter);
+        // printf("[%d] key[15-8]  = ", round_counter); print_byte(key[15/8]);
+        // printf("[%d]              ^\n", round_counter);
+
         if (KEYSIZE == 80) {
-            key[19/8] = (key[19/8] & 0xF0) | ((key[19/8] & 0x0F) ^ (round_counter & 0x1E)); // XOR k19,k18,k17,k16 with the leftmost 4 bits of the 5-bit round_counter
+            key[19/8] = (key[19/8] & 0xF0) | ((key[19/8] & 0x0F) ^ ((round_counter & 0x1E) >> 1)); // XOR k19,k18,k17,k16 with the leftmost 4 bits of the 5-bit round_counter
             key[15/8] = (key[15/8] & 0x7F) | (((key[15/8] >> 7) ^ (round_counter & 0x01)) << 7); // XOR k15 with the rightmost bit of the 5-bit round_counter
         }
         else if (KEYSIZE == 128) {
-            key[66/8] = (key[66/8] & 0xF8) | ((key[66/8] & 0x07) ^ (round_counter & 0x1C)); // XOR k66,k65,k64 with the leftmost 3 bits of the 5-bit round_counter
+            key[66/8] = (key[66/8] & 0xF8) | ((key[66/8] & 0x07) ^ ((round_counter & 0x1C) >> 2)); // XOR k66,k65,k64 with the leftmost 3 bits of the 5-bit round_counter
             key[63/8] = (key[63/8] & 0x3F) | (((key[63/8] >> 6) ^ (round_counter & 0x03)) << 6); // XOR k63,k62 with the rightmost 2 bits of the 5-bit round_counter
         }
 
+        printf("[%d] final key = ", round_counter + 1); print_key(key);
+
         // subkey = leftmost 64 bits
-        subkeys[round_counter - 1] = leftmost64(key);
+        subkeys[round_counter] = leftmost64(key);
+
+        // printf("subkey[%d] = ", round_counter); print_block(subkeys[round_counter - 1]);
     }
+
+    // Set the key back to its original value
+    // (Do this so I can use the variable name "key" in the above logic, rather than having to operate on "key_copy")
+    memcpy(key, key_copy, KEYSIZE/8);
 
     return subkeys;
 }
@@ -153,11 +216,17 @@ uint64_t add_round_key(uint64_t state, uint64_t round_key) {
  * @returns the updated 64-bit state with the s-box applied
  */
 uint64_t sbox_layer(uint64_t state) {
+    // printf("\nstate = "); print_block(state);
     uint64_t output = 0;
-    for (int i = 0; i < KEYSIZE/8; i++) {
+    for (int i = 0; i < 64; i+=4) {
         output <<= 4;
-        output |= sbox[(state >> (60 - i)) & 0x000000000000000F];
+        output |= sbox[(uint8_t)(state >> (60 - i)) & 0x000000000000000F];
+
+        // printf("\nnibble[%d-%d] = 0x%02x = ", (60 - i + 3), (60 - i), (uint8_t)((state >> (60 - i)) & 0x000000000000000F)); print_byte((uint8_t)(state >> (60 - i)) & 0x000000000000000F);
+        // printf("sbox[%d] = 0x%02x = ", (uint8_t)((state >> (60 - i)) & 0x000000000000000F), sbox[(state >> (60 - i)) & 0x000000000000000F]); print_byte(sbox[(state >> (60 - i)) & 0x000000000000000F]);
+        // printf("output = "); print_block(output);
     }
+    // printf("\nfinal output = "); print_block(output);
     return output;
 }
 
@@ -167,11 +236,16 @@ uint64_t sbox_layer(uint64_t state) {
  * @returns the updated 64-bit state with the P permutation applied
  */
 uint64_t p_layer(uint64_t state) {
+    // printf("\nstate = "); print_block(state);
     uint64_t output = 0;
     for (int i = 0; i < 64; i++) {
-        output <<= 1;
-        output |= (state >> (64 - perm[i])) & 0x0000000000000001; 
+        // Permutation read as "bit i of the input state is moved to bit position P(i) in the output state"
+        // printf("\nBit %d of the input state is moved to bit position %d of the output state\n", i, perm[i]);
+        // printf("input[%d] = %d\n", i, (state >> i) & 0x0000000000000001);
+        output |= ((state >> i) & 0x0000000000000001) << perm[i]; 
+        // printf("output = "); print_block(output);
     }
+    // printf("\nfinal output = "); print_block(output);
     return output;
 }
 
@@ -191,13 +265,20 @@ uint64_t present(uint64_t input, uint8_t *key) {
     uint64_t *subkeys = generate_round_keys(key);
 
     uint64_t state = input;
+
+    // For the sake of generating keys, the round counter starts at 1, and goes to 31 (inclusive, so 1 <= round_counter <= 31)
     for (int i = 0; i < 31; i++) {
+        printf("\nsubkey[%d] = ", i); print_block(subkeys[i]);
         state = add_round_key(state, subkeys[i]);
+        printf("[%d]add_key = ", i+1); print_block(state);
         state = sbox_layer(state);
+        printf("[%d]sbox_layer = ", i+1); print_block(state);
         state = p_layer(state);
+        printf("[%d]p_layer = ", i+1); print_block(state);
     }
 
     state = add_round_key(state, subkeys[31]);
+    printf("\n[%d]FINAL_add_key = ", 32); print_block(state);
 
     free(subkeys);
     return state;
@@ -214,30 +295,40 @@ uint64_t present(uint64_t input, uint8_t *key) {
  */
 int main() {    
     // Set test variables for the cipher
-    uint64_t plaintext = 0x9474B8E8C73BCA7D;
+    uint64_t plaintext = 0x0000000000000000;
+    // uint64_t plaintext = 0x9474b8e8c73bca7d;
 
     uint8_t key[10] = {
-        0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
-        0x08, 0x09
+        0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00
     };
+    // uint8_t key[10] = {
+    //     0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+    //     0x08, 0x09
+    // };
 
     // Encrypt
+    printf("========================\nENCRYPTION\n========================\n");
     uint64_t ciphertext = present(plaintext, key);
 
+
     // Decrypt
+    printf("========================\nDECRYPTION\n========================\n");
     uint64_t decrypted_plaintext = present(ciphertext, key);
 
     // Print results
-    printf("plaintext = %016lx\n\r", plaintext);
-    printf("ciphertext = %016lx\n\r", ciphertext);
-    printf("decrypted_plaintext = %016lx\n\r", decrypted_plaintext);
+    printf("========================\nRESULTS\n========================\n");
+    printf("plaintext = %016lx\n", plaintext);
+    printf("ciphertext = %016lx\n", ciphertext);
+    printf("decrypted_plaintext = %016lx\n", decrypted_plaintext);
 
     // Sanity check the results
     if (memcmp(&plaintext, &ciphertext, 8) == 0) {
-        printf("ERROR: Plaintext and ciphertext ARE the same!\n\r");
+        printf("ERROR: Plaintext and ciphertext ARE the same!\n");
     }
     if (memcmp(&plaintext, &decrypted_plaintext, 8) != 0) {
-        printf("ERROR: Plaintext and decrypted_plaintext are NOT the same!\n\r");
+        printf("ERROR: Plaintext and decrypted_plaintext are NOT the same!\n");
     }
 
     return 0;
